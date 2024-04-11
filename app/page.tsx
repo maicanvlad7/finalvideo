e41de6b998ee3d664ffa6dbf70e72b493468a0d5
Vladut Maican <vladutmaican@Vladuts-MacBook-Pro.local>
2024-04-11 16:54:09 +0300

    fix button main page

--- app/page.tsx
+++ app/page.tsx
@@ -30,12 +30,12 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+      <div className="">
         <h2 className="text-5xl outline-blue-600 text-shadow-sm shadow-blue-400">Final Video</h2>
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-1 lg:text-left">
-        <Button className="max-w-[300px] mx-auto">
+        <Button className="max-w-[300px] min-w-[300px] mx-auto">
           <Link href="/login" className="flex items-center gap-2 font-semibold">
             <span className="">Login</span>
           </Link>
