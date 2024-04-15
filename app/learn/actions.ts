e777c8198acb9640b5f32db17baac063c132d7d9
Vladut Maican <vladutmaican@Vladuts-MacBook-Pro.local>
2024-04-15 12:01:27 +0300

    final final fix

--- app/learn/actions.ts
+++ app/learn/actions.ts
@@ -1,8 +1,10 @@
 import { createClient } from "@/utils/supabase/server"
 
-const supabase = createClient()
+
 
 export  async function getCourses(){
+    const supabase = createClient()
+    
     const result = await supabase.from('courses').select('*')
     if(result) return result;
 }
